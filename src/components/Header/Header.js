b1ae428f301cad0a54b878fd3e0fb00b3c8b9c27
Raman Pandey <ramanpandey413@gmail.com>
2025-02-07 21:53:41 +0530

    made navbar responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { DIVL } from './singledivL.styled'
 import {DIVR} from './singledivR.styled'
 import { Styledheader } from './page.styled'
+
 const Header = () => {
   return (
     <Styledheader>
