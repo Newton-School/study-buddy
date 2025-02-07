c7024dfd6fab47076ad16c752614ff6091ad3c9c
Raman Pandey <ramanpandey413@gmail.com>
2025-02-07 17:11:48 +0530

    Navbar-initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DIVL } from './singledivL.styled'
+import {DIVR} from './singledivR.styled'
+import { Styledheader } from './page.styled'
+const Header = () => {
+  return (
+    <Styledheader>
+        <DIVL></DIVL>
+        <DIVR></DIVR>
+    </Styledheader>
+  )
+}
+
+export default Header
---
+++ src/components/Header/page.styled.js
@@ -0,0 +1,12 @@
+// import Head from "next/head";
+import styled from "styled-components";
+export const Styledheader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding:8px 16px 8px 16px;
+  height:56px;
+  width:100%;
+  position:fixed;
+  border-bottom:1px solid #E1E5EA;
+  `;
+export default Styledheader;
